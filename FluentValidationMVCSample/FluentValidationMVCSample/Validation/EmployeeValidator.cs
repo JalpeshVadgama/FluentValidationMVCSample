12d39bf96c6812b3295f5dc99557cc7db7da762c
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-18 23:38:45 +0530

    Added validator class for employee model class

---
+++ FluentValidationMVCSample/FluentValidationMVCSample/Validation/EmployeeValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidationMVCSample.Models;
+using FluentValidation;
+
+namespace FluentValidationMVCSample.Validation
+{
+    public class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.FirstName).NotNull().WithMessage("Please enter first name");
+            RuleFor(x => x.LastName).NotNull().WithMessage("Please ener last name");
+        }
+    }
+}
